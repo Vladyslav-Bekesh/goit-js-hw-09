fcb65427673ad5bf6710d154005aba0d7ca0094f
Vladyslav <dyadyavadya308@gmail.com>
2023-04-09 21:37:41 +0300

    Update 03-promises.js
    simple promise done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,12 +1,30 @@
 import Notiflix from 'notiflix';
 
+const refs = {
+  form: document.querySelector('.form'),
+  inputDelay: document.querySelector('[name="delay"]'),
+  inputStep: document.querySelector('[name="step"]'),
+  inputAmount: document.querySelector('[name="amount"]'),
+};
+refs.form.addEventListener('submit', event => {
+  event.preventDefault();
+  createPromise()
+    .then(x => {
+      Notiflix.Notify.success(`${x}`);
+    })
+    .catch(x => {
+      Notiflix.Notify.failure(`${x}`);
+    });
+});
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    if (shouldResolve) {
+      resolve('Sol lucet omnibus');
+    }
+    reject('Qui timide rogat docet negare');
+  });
+  return promise;
 }
-Notiflix.Notify.info('Sol lucet omnibus');
-setTimeout(() => {Notiflix.Notify.info('Sol lucet omnibus')}, 1000);
