17a31016ba252192817f65d6d7257775c11f3214
Vladyslav <dyadyavadya308@gmail.com>
2023-04-09 22:40:27 +0300

    Update 03-promises.js
    potuga

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,24 +6,36 @@ const refs = {
   inputStep: document.querySelector('[name="step"]'),
   inputAmount: document.querySelector('[name="amount"]'),
 };
+let timerId = null;
+
 refs.form.addEventListener('submit', event => {
   event.preventDefault();
-  createPromise()
-    .then(x => {
-      Notiflix.Notify.success(`${x}`);
-    })
-    .catch(x => {
-      Notiflix.Notify.failure(`${x}`);
-    });
+  setTimeout(() => {
+    let pos = 1;
+    while (pos <= refs.inputAmount.value) {
+      createPromise(pos, refs.inputStep.value)
+        .then(saccesfullMessage => {
+          Notiflix.Notify.success(`${saccesfullMessage}`);
+          console.log('done', pos);
+        })
+        .catch(unSaccesfullMessage => {
+          Notiflix.Notify.failure(`${unSaccesfullMessage}`);
+          console.log('not done', pos);
+        });
+      pos += 1;
+    }
+  }, refs.inputDelay.value);
 });
 
 function createPromise(position, delay) {
   const promise = new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
+    console.log(position);
 
     if (shouldResolve) {
       resolve('Sol lucet omnibus');
     }
+
     reject('Qui timide rogat docet negare');
   });
   return promise;
