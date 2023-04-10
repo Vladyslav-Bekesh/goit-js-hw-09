782174db2aa0c677418ec79ebf23030c76f0b037
Vladyslav <dyadyavadya308@gmail.com>
2023-04-10 12:15:01 +0300

    Update 03-promises.js
    some changes

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -13,30 +13,34 @@ refs.form.addEventListener('submit', event => {
   setTimeout(() => {
     let pos = 1;
     while (pos <= refs.inputAmount.value) {
+      
       createPromise(pos, refs.inputStep.value)
         .then(saccesfullMessage => {
           Notiflix.Notify.success(`${saccesfullMessage}`);
-          console.log('done', pos);
+          // console.log(`done|, pos:${pos}`);
         })
         .catch(unSaccesfullMessage => {
           Notiflix.Notify.failure(`${unSaccesfullMessage}`);
-          console.log('not done', pos);
+          // console.log(`not done|, pos:${pos}`);
         });
+      
       pos += 1;
     }
   }, refs.inputDelay.value);
 });
 
 function createPromise(position, delay) {
-  const promise = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
+
     console.log(position);
-
-    if (shouldResolve) {
-      resolve('Sol lucet omnibus');
-    }
-
-    reject('Qui timide rogat docet negare');
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`work, ${delay} pos:${position}`);
+      } else {
+        reject(`NOT work, ${delay} pos:${position}`);
+      }
+    }, delay);
   });
-  return promise;
+
 }
